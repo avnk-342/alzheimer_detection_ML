0cfe500d5bc540a14294e142ecc40aed87fb90eb
capN2 <avnk.24598@gmail.com>
2023-11-06 22:12:49 +0530

    fixed structure, adding CSS is pending

--- react_alzheimer_base/my_app/src/Components/Hero/Hero.jsx
+++ react_alzheimer_base/my_app/src/Components/Hero/Hero.jsx
@@ -8,8 +8,11 @@ const Hero = () => {
             <div className="container2">
                 <h1>Check your result <span>NOW!</span></h1>
                 <p>please upload your brain MRI scan below in jpeg, png or jpg format</p>
-                <label htmlFor="upload-image">Upload</label>
-                <input type="file" accept='image/jpeg, image/png, image/jpg' id='upload-image'/>
+                <form method='post' action='post'>
+                  <label htmlFor="upload-image">Upload</label>
+                  <input type="file" accept='image/jpeg, image/png, image/jpg' id='upload-image'/>
+                  <button type='submit' name='upload'>Upload</button>
+                </form>
             </div>
         </div>
     </div>
