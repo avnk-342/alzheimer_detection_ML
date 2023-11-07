1d8db0c786e55424b79b946eb89fc7784247842c
capN2 <avnk.24598@gmail.com>
2023-11-07 22:45:55 +0530

    added the outline to call API

--- react_alzheimer_base/my_app/src/Components/Hero/Hero.jsx
+++ react_alzheimer_base/my_app/src/Components/Hero/Hero.jsx
@@ -1,19 +1,37 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import './Hero.css'
-import UploadServices from "../Services/FileUpload.js"
+import axios from 'axios'
+
+
 
 const Hero = () => {
+  const [image, setImage] = useState('')
+  
+  const handelChange = (e) => {
+    console.log(e.target.files[0])
+    setImage(e.target.files[0])
+  }
+
+  const handleAPI = () => {
+    //API call
+    const url = ''
+    const formData = new FormData()
+    formData.append('image', image )
+    axios.post(url, formData).then((res)=>{
+      console.log(res)
+    })
+  }
+
   return (
     <div className='main-div'>
         <div className="container">
             <div className="container2">
                 <h1>Check your result <span>NOW!</span></h1>
                 <p>please upload your brain MRI scan below in jpeg, png or jpg format</p>
-                <form method='post' action='post'>
-                  <label htmlFor="upload-image">Upload</label>
-                  <input type="file" accept='image/jpeg, image/png, image/jpg' id='upload-image'/>
-                  <button type='submit' name='upload'>Upload</button>
-                </form>
+                {/* <label htmlFor="upload-image">Upload</label> */}
+                <input type="file" accept='image/jpeg, image/png, image/jpg' value={image} onChange={handelChange}/>
+                <button type='submit' name='upload' onClick={handleAPI}>Upload</button>
+                
             </div>
         </div>
     </div>
