80c45f17573713e213f499f8bd1d0ddf51a8b0b3
capN2 <avnk.24598@gmail.com>
2023-11-09 21:47:43 +0530

    added API files

---
+++ alzheimer_FastAPI/app.py
@@ -0,0 +1,47 @@
+import uvicorn
+from fastapi import FastAPI, Request, Form, UploadFile, File
+import uuid
+import numpy as np
+import pandas as pd
+import pickle
+from fastapi.middleware.cors import CORSMiddleware
+from keras.preprocessing.image import load_img, img_to_array
+
+app = FastAPI()
+
+IMAGEDIR = "image/"
+
+# application can contact with api if it is running of following port
+origins = [
+    'http://localhost:3000'  
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+)
+
+
+# Loading model using pickle
+my_model = pickle.load(open('model.pkl','rb'))
+
+
+
+@app.post('/')
+async def get_prediction(file: UploadFile = File(...)):
+    file.filename = f"{uuid.uuid4()}.jpg"
+    contents = await file.read()
+
+    #saving image
+    with open(f"{IMAGEDIR}{file.filename}", "wb") as f:
+        f.write(contents)
+
+    class_name = ['Mild Demented', 'Moderate Demented', 'Non Demented', 'Very-Mild Demented']
+    img = load_img(f"image/{file.filename}", target_size=(128, 128))
+    img = img_to_array(img)
+    img = np.expand_dims(img, axis = 0)
+    prediction = my_model.predict(img)
+    yclasses = prediction.argmax(axis=-1)
+    ans = class_name[yclasses[0]]
+    
+    return {"Msg" : f"{ans}"}
+    
---
+++ alzheimer_FastAPI/new_model.py
@@ -0,0 +1,55 @@
+import tensorflow as tf
+import keras
+from tensorflow.keras import models, datasets, layers
+import numpy as np
+from keras.layers import Dropout
+import pickle
+
+
+
+train_set = keras.utils.image_dataset_from_directory(
+    "./output/train",
+    seed = 123,
+    batch_size = 32,
+    image_size = (128,128)
+)
+test_set = keras.utils.image_dataset_from_directory(
+    "./output/test",
+    seed = 123,
+    batch_size = 32,
+    image_size = (128,128)
+)
+val_set = keras.utils.image_dataset_from_directory(
+    "./output/train",
+    seed = 123,
+    batch_size = 32,
+    image_size = (128,128)
+)
+
+
+cnn = tf.keras.models.Sequential()
+
+cnn.add(keras.layers.experimental.preprocessing.Rescaling(scale=1./255, offset=-1, input_shape=(128,128, 3)))
+
+cnn.add(tf.keras.layers.Conv2D(filters = 16, kernel_size = 3, activation = "relu",padding='same',kernel_initializer="he_normal"))
+cnn.add(tf.keras.layers.MaxPool2D((2,2)))
+
+cnn.add(tf.keras.layers.Conv2D(filters = 32, kernel_size = 3, activation = "relu",padding='same',kernel_initializer="he_normal"))
+cnn.add(tf.keras.layers.MaxPool2D((2,2)))
+
+cnn.add(keras.layers.Dropout(0.20))
+
+cnn.add(tf.keras.layers.Conv2D(filters = 64, kernel_size = 3, activation = "relu",padding='same',kernel_initializer="he_normal"))
+cnn.add(tf.keras.layers.MaxPool2D((2,2)))
+
+cnn.add(keras.layers.Dropout(0.25))
+cnn.add(tf.keras.layers.Flatten())
+
+cnn.add(tf.keras.layers.Dense(units = 128, activation = 'relu',kernel_initializer="he_normal"))
+cnn.add(tf.keras.layers.Dense(units = 64, activation = 'relu'))
+cnn.add(tf.keras.layers.Dense(units = 4, activation = 'softmax'))
+
+cnn.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+history = cnn.fit(train_set, validation_data = val_set, epochs = 15,batch_size=32,verbose=1 )
+
+pickle.dump(cnn, open('model.pkl', 'wb'))
